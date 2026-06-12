42e679f2787deadca40fda3a13c20e3ab23c5220
UshcAriel <aushca@est.ups.edu.ec>
2026-06-12 10:28:31 -0500

    Listo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import utils.QueuePalindrome;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        QueuePalindrome evaluator = new QueuePalindrome();
+
+        
+        String palabra1 = "ana";
+        String  palabra2 = "radar";
+        String palabra3 = "java";
+
+        // 3. Evaluamos e imprimimos los resultados en consola
+        System.out.println("Input: \"" + palabra1 + "\"");
+        System.out.println("Output: " + evaluator.isPalindrome(palabra1));
+        System.out.println();
+
+        System.out.println("Input: \"" + palabra2 + "\"");
+        System.out.println("Output: " + evaluator.isPalindrome(palabra2));
+        System.out.println();
+
+        System.out.println("Input: \"" + palabra3 + "\"");
+        System.out.println("Output: " + evaluator.isPalindrome(palabra3));
     }
 }
--- src/utils/QueuePalindrome.java
+++ src/utils/QueuePalindrome.java
@@ -1,9 +1,39 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class QueuePalindrome {
 
-    public boolean isValing(String s){
-        return true;
+    public boolean isPalindrome(String text) {
+       
+        if (text == null) {
+            return false;
+        }
+        
+       
+        String cleanText = text.toLowerCase();
+        
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        
+ 
+        for (int i = 0; i < cleanText.length(); i++) {
+            char c = cleanText.charAt(i);
+            queue.add(c);  
+            stack.push(c); 
+        }
+        
+        
+        while (!queue.isEmpty()) {
+
+            if (!queue.poll().equals(stack.pop())) {
+                return false; 
+            }
+        }
+        
+        return true; 
     }
 
 }
