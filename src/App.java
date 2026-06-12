a46c01509aec0ef989568c956e2e14f3230f7e1c
UshcAriel <aushca@est.ups.edu.ec>
2026-06-12 10:42:57 -0500

    Listo

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.util.Stack;
 import utils.QueuePalindrome;
+import utils.StackSorter;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -20,5 +22,22 @@ public class App {
 
         System.out.println("Input: \"" + palabra3 + "\"");
         System.out.println("Output: " + evaluator.isPalindrome(palabra3));
+
+        StackSorter sorter = new StackSorter();
+        Stack<Integer> miStack = new Stack<>();
+
+        // Llenamos el stack de abajo hacia arriba.
+        // Para que el tope sea 5, debemos meterlos en orden inverso al input visual:
+        miStack.push(2);
+        miStack.push(4);
+        miStack.push(1);
+        miStack.push(5); // Este queda en el tope
+
+        System.out.println("Input original: (tope) " + miStack);
+
+        // Ordenamos el stack
+        sorter.sortStack(miStack);
+
+        System.out.println("Output ordenado: (tope) " + miStack);
     }
 }
--- src/utils/StackSorter.java
+++ src/utils/StackSorter.java
@@ -1,6 +1,32 @@
 package utils;
 
+import java.util.Stack;
+
 public class StackSorter {
-    
+    public void sortStack(Stack<Integer> stack) {
+
+        if (stack == null || stack.isEmpty()) {
+            return;
+        }
+
+        Stack<Integer> auxStack = new Stack<>();
+
+   
+        while (!stack.isEmpty()) {
+
+            int current = stack.pop();
+
+ 
+            while (!auxStack.isEmpty() && auxStack.peek() > current) {
+                stack.push(auxStack.pop());
+            }
+
+            auxStack.push(current);
+        }
+
+        while (!auxStack.isEmpty()) {
+            stack.push(auxStack.pop());
+        }
+    }
 
 }
